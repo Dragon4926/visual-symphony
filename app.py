6067c8d79aeb489f895943cfa8e6ad3e13a1adf8
Debopriyo Das <70844598+Dragon4926@users.noreply.github.com>
2025-01-19 21:14:51 +0530

    story-gen done ✅

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ tf.get_logger().setLevel('ERROR')  # Only show errors, not warnings
 
 from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline, logging
+from langchain import PromptTemplate, LLMChain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
 
 # Set transformers logging to error only
 logging.set_verbosity_error()
@@ -27,7 +30,33 @@ def image2text(url):
 
     result = pipe(url, max_new_tokens=20)[0]['generated_text']  
     
-    print(result)
+    # print(result)
     return result
 
-image2text("im1.jpg")
+#LLM
+def gen_story(scene):
+    template = [
+        ("system", "You are an experienced webnovel writer."),
+        ("human", """Create an enchanting tale in under 20 words about: {scene}.""")
+    ]
+
+    prompt = ChatPromptTemplate.from_messages(template)
+    
+    llm = ChatGroq(
+        temperature=1,
+        groq_api_key=os.getenv("GROQ_API_KEY"),
+        model_name="mixtral-8x7b-32768"
+    )
+    
+    # Created the chain 
+    chain = prompt | llm
+    
+    # Get story response
+    response = chain.invoke({"scene": scene})
+    return response.content
+
+#tts
+
+if __name__ == "__main__":
+    story = gen_story(image2text("im1.jpg")).strip("\"")
+    print(story)
